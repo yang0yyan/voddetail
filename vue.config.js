7f9a7d058a79e3d30824f0e663a7377f836aa5ff
yang0yyan <58495156+yang0yyan@users.noreply.github.com>
2023-05-25 13:36:53 +0800

    Update vue.config.js
    yy

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: {
-      "/service-Ccteg": {
-        target: "http://10.7.1.94:10033/service-ccteg",
-        changeOrigin: true,
-        secure: false,
-        pathRewrite: {
-          "^/service-Ccteg": "",
-        },
-      },
-    },
+    
   },
 });
